c21d0a38126a69b371490076e2f7c3b8983db8cd
shakil-ahmmed-se <mdshakilahmmed8906@gmail.com>
2024-09-02 03:00:12 +0600

    add axios

--- src/compoenents/TopHeadLine/TopHeadLine.js
+++ src/compoenents/TopHeadLine/TopHeadLine.js
@@ -1,16 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import News from '../News/News';
+import axios from 'axios';
 
 const TopHeadLine = () => {
 
     const [articles, setArticle] = useState([])
 
-    useEffect(()=> {
-        // Fetch top headline news from API
-        // and display it on the page
-        fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=2c4d553cdf344431951f181d49422e2c')
-       .then(response => response.json())
-       .then(data => setArticle(data.articles));
+    // useEffect(()=> {
+    //     fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=2c4d553cdf344431951f181d49422e2c')
+    //    .then(response => response.json())
+    //    .then(data => setArticle(data.articles));
+    // },[])
+
+    useEffect(()=>{
+        axios('https://newsapi.org/v2/top-headlines?country=us&apiKey=2c4d553cdf344431951f181d49422e2c')
+        .then(data => setArticle(data.data.articles))
     },[])
     return (
         <div>
